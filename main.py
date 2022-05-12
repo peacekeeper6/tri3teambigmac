703297b935be882a7b996f9d63ecf968faefac2b
Aidan Wu <aidanywu@gmail.com>
2022-05-12 13:56:12 -0700

    removed crud api

--- main.py
+++ main.py
@@ -2,11 +2,11 @@ from flask import render_template
 from __init__ import app
 
 from cruddy.app_crud import app_crud
-from cruddy.app_crud_api import app_crud_api
+# from cruddy.app_crud_api import app_crud_api
 
 
 app.register_blueprint(app_crud)
-app.register_blueprint(app_crud_api)
+# app.register_blueprint(app_crud_api)
 
 
 # connects default URL to render index.html
